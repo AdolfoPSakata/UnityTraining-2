7e991e00964f7d74fd6a76463bebd9c8cd68e9a8
Adolfo Sakata <asakata@kokku.com.br>
2023-12-11 15:03:34 -0300

    Player behavior
    Initial Features and Coroutine to test

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+    public UnityAction JumpAction;
+
+    Rigidbody2D rb;
+    [SerializeField] private float jumpForce = 10f;
+
+    public Player()
+    {
+        JumpAction = Jump;
+    }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Die();
+        }
+    }
+}
